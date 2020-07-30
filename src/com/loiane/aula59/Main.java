0a6bbbfabb755bf1e49317ef479101deb7b5a0d0
kodom <sampaiorodrigomoreira@gmail.com>
2020-07-29 22:15:53 -0300

    Aula 59

---
+++ src/com/loiane/aula59/Main.java
@@ -0,0 +1,20 @@
+package com.loiane.aula59;
+
+//adicionar a palavra static
+import static java.lang.Math.pow;
+import static java.lang.StrictMath.sqrt;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// importacao estatica
+
+        double a = 2 ;
+        double b = 3;
+        double c = 4;
+
+        System.out.println(pow(a, b));
+        System.out.println(sqrt(c));
+
+    }
+}
