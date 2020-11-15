15057513cbee42a6f3eef316c91b8f6bd48dd9ff
kodomosam <sampaiorodrigomoreira@gmail.com>
2020-11-15 16:22:24 -0300

    aula 10

---
+++ src/com/loiane/aula10/Variaveis.java
@@ -0,0 +1,14 @@
+package com.loiane.aula10;
+
+public class Variaveis {
+
+    public static void main(String[] args){
+
+        int idade = 20;
+        String nome = "Rodrigo";
+        String nomeDoMeuCachorro;
+
+        idade = 25;
+        
+    }
+}
