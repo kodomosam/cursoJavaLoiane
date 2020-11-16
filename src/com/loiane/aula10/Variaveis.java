6f78353b3b8f13f35455227ff1b40f2658fad9e3
kodomosam <sampaiorodrigomoreira@gmail.com>
2020-11-16 10:41:19 -0300

    aula 11

--- src/com/loiane/aula10/Variaveis.java
+++ src/com/loiane/aula10/Variaveis.java
@@ -9,6 +9,6 @@ public class Variaveis {
         String nomeDoMeuCachorro;
 
         idade = 25;
-        
+
     }
 }
---
+++ src/com/loiane/aula11/VariaveisInteiros.java
@@ -0,0 +1,9 @@
+package com.loiane.aula11;
+
+public class VariaveisInteiros {
+
+    byte idade01 = 21;
+
+
+
+}
